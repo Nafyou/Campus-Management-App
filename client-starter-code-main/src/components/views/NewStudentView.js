b7e9cfbbde916107c4132ea0d61f54073109484a
xNafew <Nafew.Mustafa94@myhunter.cuny.edu>
2022-07-14 16:11:31 -0400

    Fixed New Student

--- client-starter-code-main/src/components/views/NewStudentView.js
+++ client-starter-code-main/src/components/views/NewStudentView.js
@@ -61,7 +61,19 @@ const NewStudentView = (props) => {
             <br/>
             <br/>
 
-            <label style={{color:'#11153e', fontWeight: 'bold'}}>Campus Id: </label>
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>email: </label>
+            <input type="text" name="email" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>gpa: </label>
+            <input type="text" name="gpa" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+                        
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Campus ID: </label>
             <input type="text" name="campusId" onChange={(e) => handleChange(e)} />
             <br/>
             <br/>
--- client-starter-code-main/src/components/views/StudentView.js
+++ client-starter-code-main/src/components/views/StudentView.js
@@ -11,8 +11,10 @@ const StudentView = (props) => {
   return (
     <div>
       <h1> Student Profile </h1>
-      <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <h2>Student Name: {student.firstname + " " + student.lastname}</h2>
+      <h3>Campus Name: {student.campus.name}</h3>
+      <h3>GPA: {student.gpa}</h3>
+      <h3>Email: {student.email}</h3>
     </div>
   );
 
